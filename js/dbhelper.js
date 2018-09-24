776e50c40be7c98510636cf51e528c14cb16253e
Amy Carney <click2carney@yahoo.com>
2018-09-23 19:22:00 -0800

    change 5-stars to radios; create reviews store in IDB

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -15,12 +15,23 @@ class DBHelper {
    * open cache
   **/
   static get IDB() {
-    const dbPromise = idb.open('reviews-db', 1, (upgradeDB) => {
+    const dbPromise = idb.open('reviews-db', 2, (upgradeDB) => {
       // create object store
       switch (upgradeDB.oldVersion) {
         case 0:
+          const restaurantStore = upgradeDB.createObjectStore('restaurants', {
+            keyPath: 'id'
+          });
+          restaurantStore.createIndex('cuisine', 'cuisine_type');
+          restaurantStore.createIndex('neighborhood', 'neighborhood');
         case 1:
-          upgradeDB.createObjectStore('restaurants', {keyPath: 'id'});
+          const reviewStore = upgradeDB.createObjectStore('reviews', {
+            keyPath: 'id'
+          });
+          reviewStore.createIndex('restaurant', 'restaurant_id');
+          upgradeDB.createObjectStore('reviewQueue', {
+            autoIncrement: true
+          });
       }
     });
     return dbPromise;
@@ -49,6 +60,31 @@ class DBHelper {
       callback(error, null);
     });
   }
+
+  /**
+   * Fetch and cache restaurant reviews.
+   */
+  static fetchReviews(id) {
+    fetch(`${this.DATABASE_URL}reviews/?restaurant_id=${id}`)
+    .then(response => response.json())
+    .then(reviews => {
+      this.IDB
+      .then( db => {
+        const tx = db.transaction('reviews', 'readwrite'),
+              reviewStore = tx.objectStore('reviews');
+        reviews.forEach( review => {
+          reviewStore.put(review);
+        });
+        callback(null, reviews);
+        return tx.complete;
+      });
+    })
+    .catch((error) => {
+      console.log(`Request failed: ${error}`);
+      callback(error, null);
+    });
+  }
+
 
   /**
    * Fetch a restaurant by its ID.
