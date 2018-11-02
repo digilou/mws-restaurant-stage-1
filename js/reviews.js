849fbd35163c58fe766d2f99c8cafd584228f028
Amy Carney <click2carney@yahoo.com>
2018-11-01 20:19:42 -0800

    add if statement, event listener, and idb code

--- js/reviews.js
+++ js/reviews.js
@@ -24,40 +24,82 @@ function addReview(event) {
 
   // POST the review to the server if online, which then puts in IDB
   // Otherwise, post to reviews queue if offline, then post to server when online
-  if(window.navigator.onLine) {
-    postToServer(reviewData);
-  } else {
-    addReviewToQueue()
-    .then(postToServer(reviewData));
-  }
+  window.navigator.onLine ? postToServer(reviewData) : addReviewToQueue();
 
   // Refresh page (in a hacky sort of way) to re-populate reviews
   window.navigator.onLine ? window.location.reload(true) : window.location.reload(false);
 
   // Clear/reset the form fields
   // reviewForm.reset();
+  
+  // listen for online status
+  window.addEventListener('online', () => {
+    console.log("Back online!");
+  });
 }
 
 
 // STEPS
 // 1. Check if user is online
 // 2. If offline, capture data in IDB reviewsQueue
+// Get database object
+// Open transaction on database
+// Open object store on transaction (reviewQueue)
+// Perform operation on object store...
+// reviews.forEach(review => reviewQueue.put(review))
 // 3. Notify user they are offline
-// 4. Else, postToServer()
-// 5. When back online, postToServer
-// 6. And copy reviewQueue data to IDB reviews
+// .then(notifyUser(message))
+// pop-up/modal
+// or console.log() ?
+// 4. Post review on page (even when offline)
+// createReviewHTML()
+// 5. Else, postToServer()
+// So, when I want to save a review, I do a check (if statement) to see if online.  If offline, save to offline reviews.  If online, save to server (which then updates the online reviews DB
+// and then I have an event listener that listens for ‘online’
+// and runs the online handler function that tries to update reviews/favorites
+// 5. When back online, postToServer()
+// 6. Which copies reviewQueue data to IDB reviews
 // 7. Clear reviewQueue data
 // 8. Refresh page
+
+/**
+ * Modal with notification for user
+ */
+
+ function modalMsg() {
+   // create a dialog
+   // Your review will be posted when you are online.
+   // a11y
+ }
+
+
+/**
+ * Notify user that they are offline
+ */
+
+ function notifyUser() {
+   console.log('Looks like you\'re offline!');
+   modalMsg();
+ }
 
 /**
  * Add to queue
  */
 function addReviewToQueue(data) {
-  DBHelper.openDb.then(db => {
-    const queueStore = db.transaction('reviewQueue', 'readwrite').objectStore('reviewQueue');
-    return queueStore.openCursor();
+  notifyUser()
+  .then(reviews => {
+    DBHelper.openDb.then( db => {
+      const reviewStore = db.transaction('reviewQueue', 'readwrite').objectStore('reviewQueue');
+      reviews.forEach(
+        review => reviewStore.put(review)
+      );
+      return reviewStore.complete;
+    });
   })
-  .then(DBHelper.postNextReview());
+  .then(console.log('Reviews queued!'))
+  .then(createReviewHTML())
+  .then(reviewStore.clear())
+  // .then(DBHelper.postNextReview());
 }
 
 /**
